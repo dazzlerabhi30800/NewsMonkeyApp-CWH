c3011cde7e17f6b5adcc6ae1d67a488b99dde690
dazzlerabhi30800 <abhisingh30800@gmail.com>
2022-09-08 19:56:32 +0530

    added prev and next button

--- src/Components/News.js
+++ src/Components/News.js
@@ -8,29 +8,77 @@ export default class News extends Component {
         this.state = {
             loading: true,
             articlesData: [],
+            page: 1,
         }
     }
     async componentDidMount() {
-        let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=9e1bd06f25544f399975122857782f19";
+        this.setState({ loading: true });
+        let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=9e1bd06f25544f399975122857782f19&pageSize=19";
         let data = await fetch(url);
         let parsedData = await data.json();
-        this.setState({ articlesData: parsedData.articles });
+        this.setState({ articlesData: parsedData.articles, totalResults: parsedData.totalResults, loading: false });
+    }
+    handlePrevClick = async () => {
+        console.log("previous button Clicked!")
+        this.setState({ loading: true });
+        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=9e1bd06f25544f399975122857782f19&page=${this.state.page - 1}&pageSize=19`;
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        this.setState({
+            page: this.state.page - 1,
+            articlesData: parsedData.articles,
+            loading: false,
+        });
+    }
+    handleNextClick = async () => {
+        console.log("Next button Clicked!")
+        this.setState({ loading: true })
+        if (this.state.page + 1 > Math.ceil(this.state.totalResults / 20)) {
+            return; // stop the function from running
+        }
+        else {
+            let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=9e1bd06f25544f399975122857782f19&page=${this.state.page + 1}&pageSize=19`;
+            let data = await fetch(url);
+            let parsedData = await data.json();
+            this.setState({
+                page: this.state.page + 1,
+                articlesData: parsedData.articles,
+                loading: false,
+            })
+        }
     }
     render() {
         console.log('render')
-        const newsComp = this.state.articlesData.map((article, index) => {
-            return (
-                <NewsItem
-                    item={article}
-                    key={index}
-                />
-            )
-        })
+        // const newsComp = this.state.articlesData.map((article, index) => {
+        //     return (
+        //         <NewsItem
+        //             item={article}
+        //             key={index}
+        //         />
+        //     )
+        // })
         return (
-            <div className='p-4'>
-                <h1 className='font-bold text-indigo-800 text-xl text-center'>NewsMonkey - Top Headlines</h1>
-                <div className='flex flex-wrap gap-6 my-4 w-full justify-center'>
-                    {newsComp}
+            <div className='p-4 flex flex-col'>
+                <h1 className='font-bold text-indigo-800 text-3xl text-center'>NewsMonkey - Top Headlines</h1>
+                <div className='flex flex-wrap w-full justify-center mx-auto gap-6 p-4 md:gap-10 my-4'>
+                    {this.state.loading ?
+                        <p className='text-3xl text-red-800'>News is Fetching...</p>
+                        :
+                        (
+                            this.state.articlesData.map((article, index) => {
+                                return (
+                                    <NewsItem
+                                        item={article}
+                                        key={index}
+                                    />
+                                )
+                            })
+                        )
+                    }
+                </div>
+                <div className="button--container w-3/4 mx-auto self-center flex justify-around p-2 px-4">
+                    <button disabled={this.state.page <= 1} onClick={this.handlePrevClick} className='py-2 px-6 disabled:opacity-50 bg-amber-500 shadow-md cursor-pointer transition ease-in duration-300 hover:bg-amber-300 text-white font-bold'>&larr; Prev</button>
+                    <button disabled={this.state.page >= 2} onClick={this.handleNextClick} className='py-2 px-6 bg-lime-500 disabled:opacity-50 shadow-md cursor-pointer transition ease-in duration-300 hover:bg-lime-300 text-white font-bold'>Next &rarr;</button>
                 </div>
             </div>
         )
